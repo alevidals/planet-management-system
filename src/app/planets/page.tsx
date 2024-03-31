1a6d1b6bdd2db088a1a8a250b25be1fd24cc7110
alevidals <alejandro.vidal.sanchez16@gmail.com>
2024-03-31 12:08:03 +0200

    feat: add basic layout

--- src/app/planets/page.tsx
+++ src/app/planets/page.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function PlanetsPage() {
   return (
     <main className="bg-zinc-900 min-h-screen container mx-auto">
       <h1 className="text-primary">PMS</h1>
-      <h2>Home Page</h2>
-      <Link className="underline" href="/planets">
-        Go to planets
+      <h2>Planets Page</h2>
+      <Link className="underline" href="/">
+        Go to home
       </Link>
     </main>
   );
