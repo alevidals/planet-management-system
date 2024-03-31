d1bfc51841bf698727a70df689cca57c167d2d93
alevidals <alejandro.vidal.sanchez16@gmail.com>
2024-03-31 12:26:27 +0200

    chore: change planets lists component to render information about the planet

--- src/components/planets-list.tsx
+++ src/components/planets-list.tsx
@@ -1,4 +1,5 @@
 import type { getPlanets } from "@/lib/planets";
+import Link from "next/link";
 
 type Props = {
   planets: Awaited<ReturnType<typeof getPlanets>>;
@@ -7,9 +8,17 @@ type Props = {
 export function PlanetsList({ planets }: Props) {
   return (
     <div>
-      <ul>
+      <ul className="grid grid-cols-3 gap-4">
         {planets.map((planet) => (
-          <li key={planet.id}>{planet.name}</li>
+          <li className="bg-zinc-950 p-4 rounded-lg" key={planet.id}>
+            <Link href={`/planets/${planet.id}`}>
+              <p>{planet.name}</p>
+              <p>{planet.diameter}</p>
+              <p>{planet.climates.join(",")}</p>
+              <p>{planet.terrains.join(",")}</p>
+              <p>{planet.numberOfResidents}</p>
+            </Link>
+          </li>
         ))}
       </ul>
     </div>
